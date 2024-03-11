d7f7e99538afdc404fd4a5799203fecdf2643586
Danish Jamal <mddanishjamal04@yahoo.com>
2024-03-11 15:50:01 +0530

    changes in the counter app

--- counter-app/src/App.jsx
+++ counter-app/src/App.jsx
@@ -1,13 +1,16 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
   const [second, setSecond] = useState(0);
   const [minute, setMinute] = useState(0);
   const [start, setStart] = useState(false);
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [timerTicked, setTimerTicked] = useState(false);
 
   const handleStart = () => {
     setStart(true);
+    setTimerStarted(true);
   };
 
   const handleStop = () => {
@@ -18,30 +21,44 @@ function App() {
     setSecond(0);
     setMinute(0);
     setStart(false);
+    setTimerStarted(false);
+    setTimerTicked(false);
   };
 
   useEffect(() => {
-    const timer = setInterval(() => {
-      if (start) {
-        setSecond((prevSecond) => {
-          if (prevSecond === 59) {
-            setMinute((prevMinute) => prevMinute + 1);
+    let intervalId;
+    if (start) {
+      intervalId = setInterval(() => {
+        setSecond(prevSecond => {
+          if (prevSecond === 10) {
+            if (!timerTicked) {
+              setTimerTicked(true);
+            }
             return 0;
+          } else {
+            return prevSecond + 1;
           }
-          return prevSecond + 1;
         });
-      }
-    }, 1000);
+      }, 1000);
+    }
 
-    return () => clearInterval(timer);
-  }, [start]);
+    return () => clearInterval(intervalId);
+  }, [start, timerTicked]);
+
+  useEffect(() => {
+    if (timerTicked && second === 0) {
+      setMinute(prevMinute => prevMinute + 1);
+    }
+  }, [second, timerTicked]);
+
   return (
     <div className='main-div'>
       <div className='timer-div'>
-        <span className='timer'>{minute < 10 ? `${"0" + minute}` : minute}</span>:<span className='timer'>{second < 10 ? `${"0" + second}` : second}</span>
+        <span className='timer'>{minute < 10 ? `0${minute}` : minute}</span>:
+        <span className='timer'>{second < 10 ? `0${second}` : second}</span>
       </div>
       <div className='btn-container'>
-        <button className='btn' onClick={handleStart}>
+        <button className='btn' onClick={handleStart} disabled={start}>
           Start
         </button>
         <button className='btn' onClick={handleStop}>
@@ -52,7 +69,7 @@ function App() {
         </button>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
